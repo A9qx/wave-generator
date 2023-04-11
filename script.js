af752361abcc1adc02cbd9f2ae34ab6e16ac1dcd
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-04-11 10:09:46 -0600

    Added functionality to the website

--- script.js
+++ script.js
@@ -6,8 +6,37 @@ var context = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 200;
 
-// Draw the wave
-function drawWave(amplitude, frequency, color) {
+// Get the input elements and their values
+var amplitudeInput = document.getElementById("amplitude");
+var frequencyInput = document.getElementById("frequency");
+var colorInput = document.getElementById("color");
+
+var amplitudeValue = document.getElementById("amplitudeValue");
+var frequencyValue = document.getElementById("frequencyValue");
+
+// Update the wave properties when the input values change
+amplitudeInput.addEventListener("input", function() {
+  amplitudeValue.textContent = this.value;
+  drawWave();
+});
+
+frequencyInput.addEventListener("input", function() {
+  frequencyValue.textContent = this.value;
+  drawWave();
+});
+
+colorInput.addEventListener("input", function() {
+  drawWave();
+});
+
+// Draw the wave with the current properties
+function drawWave() {
+  var amplitude = amplitudeInput.value;
+  var frequency = frequencyInput.value;
+  var color = colorInput.value;
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  
   context.beginPath();
   context.strokeStyle = color;
   for (var x = 0; x <= canvas.width; x++) {
@@ -21,7 +50,5 @@ function drawWave(amplitude, frequency, color) {
   context.stroke();
 }
 
-// Call the drawWave function with different parameters to create different waves
-drawWave(50, 1, "red");
-drawWave(30, 2, "green");
-drawWave(10, 3, "blue");
+// Call the drawWave function to draw the initial wave
+drawWave();
